ab33be32ac66d45fb33fbd8baccc4982ec621a10
deep <deep.tiwari1@gmail.com>
2020-03-27 22:54:05 +0530

    added new project

---
+++ DjangoForBeginners/django-practice/pages/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+from django.http import HttpResponse
+# Create your views here.
+'''
+def homePageView(request):
+    return HttpResponse("Hello World")
+'''
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
