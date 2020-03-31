ca1bbc3054ee75ff3032a1412b6c6da4c85acc2f
deep <deep.tiwari1@gmail.com>
2020-04-01 00:25:06 +0530

    updated

--- DjangoForBeginners/message_board/posts/views.py
+++ DjangoForBeginners/message_board/posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from django.views.generic import Listview
+
+from .models import Post
+
 # Create your views here.
+
+class HomePageView(Listview):
+    model = Post
+    template_name =  'home.html'
