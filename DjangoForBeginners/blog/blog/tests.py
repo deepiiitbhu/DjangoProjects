83533b9963033afe0e31213c082dafbaeade211f
deep <deep.tiwari1@gmail.com>
2020-04-05 10:15:21 +0530

    Added the project

--- DjangoForBeginners/blog/blog/tests.py
+++ DjangoForBeginners/blog/blog/tests.py
@@ -1,3 +1,45 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+from django.urls import reverse
+
+
+from .models import Post
 
 # Create your tests here.
+
+class BlogTest(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username ='testuser',
+            email = 'test@mail.com',
+            password = 'secret'
+        )
+
+        self.post = Post.objects.create(
+            title = 'A good titile',
+            body = "Nice body  i am testing it",
+            author = self.user,
+        )
+
+    def test_string_representation(self):
+        post = Post(title='A sample title')
+        self.assertEqual(str(post), post.title)
+
+    def test_post_content(self):
+        self.assertEqual(f'{self.post.title}', 'A good titile')
+        self.assertEqual(f'{self.post.body}', 'Nice body  i am testing it')
+        self.assertEqual(f'{self.post.author}', 'testuser')
+
+    def test_post_list_view(self):
+        response =  self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Nice body  i am testing it')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_post_detail_view(self):
+        response =  self.client.get('/post/1/')
+        no_response =  self.client.get('/post/10000/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, 'Nice body  i am testing it')
+        self.assertTemplateUsed(response, 'post_detail.html')
