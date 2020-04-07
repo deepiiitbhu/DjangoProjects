a0708a15f5aae8127f3b018183b09b290058f960
deep <deep.tiwari1@gmail.com>
2020-04-07 12:20:58 +0530

    aaded user authentication

--- DjangoForBeginners/blog/blog_project/settings.py
+++ DjangoForBeginners/blog/blog_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'blog.apps.AccountsConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,3 +124,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+LOGIN_REDIRECT_URL='home'
+LOGOUT_REDIRECT_URL='home'
