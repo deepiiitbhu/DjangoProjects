7ec8e10c666e98b1d87a4e74496a6539e3e31557
deep <deep.tiwari1@gmail.com>
2020-04-15 17:37:23 +0530

    Added project with user registration

--- DjangoForProfessionals/code_dokcer_demo/hello/hello_project/urls.py
+++ DjangoForProfessionals/code_dokcer_demo/hello/hello_project/urls.py
@@ -1,7 +1,7 @@
-"""newspaper_project URL Configuration
+"""hello_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,14 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 from django.urls import path, include
-from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
-    path('users/', include('django.contrib.auth.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
-   
+    path('', include('pages.urls')),
 ]
