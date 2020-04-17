9c1dc7de776fffd2b72a39b8877d5240e6121bf9
deep <deep.tiwari1@gmail.com>
2020-04-17 15:42:45 +0530

    Added testcase

--- DjangoForProfessionals/chap3_bookstore/books/users/admin.py
+++ DjangoForProfessionals/chap3_bookstore/books/users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomerCreationForm, CustomerChangeForm
 
 # Register your models here.
+
+CustomUser =  get_user_model()
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomerCreationForm
+
+    form =  CustomerChangeForm
+    model = CustomUser
+    list_display = ['email', 'username',]
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- DjangoForProfessionals/chap3_bookstore/books/users/tests.py
+++ DjangoForProfessionals/chap3_bookstore/books/users/tests.py
@@ -1,3 +1,24 @@
+
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 # Create your tests here.
+
+class CustomerTests(TestCase):
+    def test_create_user(self):
+        user =  User.objects.create_user(
+            username = 'will',
+            email = 'will.gmail.com',
+            password='testpass123'
+        )
+
+        self.assertEqual(user.username, 'will')
+        self.assertEqual(user.email, 'will@gmail.com')
+        self.assertTrue(user.is_active)
+        self.assertTrue(user.is_staff)
+        self.assertFalse(user.is_superuser)
+        )
+
+
+
+
